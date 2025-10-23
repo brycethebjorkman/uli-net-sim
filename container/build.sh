31fe282ad799d2a13988439db7d2b4f7cb219d63
brycethebjorkman <46073756+brycethebjorkman@users.noreply.github.com>
2025-10-23 14:51:47 -0500

    Fix container build script

--- container/build.sh
+++ container/build.sh
@@ -6,6 +6,7 @@ cd $PROJ_DIR
 opp_makemake -f --deep \
     -KINET4_5_PROJ=$BASE_DIR/inet4.5 \
     -DINET_IMPORT \
+    -Isrc \
     -I$\(INET4_5_PROJ\)/src \
     -L$\(INET4_5_PROJ\)/out/clang-release/src \
     -lINET$\(D\)

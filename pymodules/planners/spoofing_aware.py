1a5b5bfa6ac3dbd0088d6429a091237ceda47615
brycethebjorkman <46073756+brycethebjorkman@users.noreply.github.com>
2026-03-12 11:04:48 -0500

    Stub out a new SpoofingAwarePlanner pymodule

---
+++ pymodules/planners/spoofing_aware.py
@@ -0,0 +1,38 @@
+import random
+
+class SpoofingAwarePlanner:
+    """ Performs spoofing localization and uses a risk-domain approach to plan federate trajectories """
+
+    def __init__(self):
+        self._federate_ids = set()
+        self.rng = random.Random(42)
+
+    def on_tick(self, data):
+        host_ids = data['host_ids']
+        commands = {}
+        for hid in host_ids:
+            some_val = self.rng.uniform(self.alt_min, self.alt_max)
+            commands[hid] = {'value_computed': some_val}
+
+        return {
+            'commands': commands,
+            'log': {
+                'tick_count': data.get('tick_count', 0),
+            },
+        }
+
+    def on_reports(self, data):
+        serial = data['serial_number']
+        reports = data['reports']
+
+        for r in reports:
+            self._federate_ids.add(r['host_id'])
+
+        some_val = self.rng.uniform(self.alt_min, self.alt_max)
+
+        return {
+            'log': {
+                'serial_seen': serial,
+                'value_computed': some_val,
+            },
+        }
